db848cee6d1954f07848b09f4222180bb6405f5e
Stefan Bruggmann <mail@newgen.ch>
2019-02-27 09:53:06 +0100

    TASK: Add version 5 drivers again and limit the index build to configured dimension combinations

--- Classes/Driver/Version1/IndexDriver.php
+++ Classes/Driver/Version1/IndexDriver.php
@@ -60,4 +60,20 @@ class IndexDriver extends AbstractDriver implements IndexDriverInterface
 
         return is_array($treatedContent) ? array_keys($treatedContent) : [];
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function indexesByPrefix($alias)
+    {
+        $response = $this->searchClient->request('GET', '/_alias/' . $alias . '*');
+        if ($response->getStatusCode() !== 200 && $response->getStatusCode() !== 404) {
+            throw new Exception('The alias prefix "' . $alias . '" was not found with some unexpected error... (return code: ' . $response->getStatusCode() . ')', 1383650137);
+        }
+
+        // return empty array if content from response cannot be read as an array
+        $treatedContent = $response->getTreatedContent();
+
+        return is_array($treatedContent) ? array_keys($treatedContent) : [];
+    }
 }
